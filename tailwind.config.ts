b69c99f929a6d344118b7bcaf5a6309dac320c97
sevenninhlee <lephuocninh7118@gmail.com>
2023-09-05 15:06:40 +0700

    Init Project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      spaceGrotesk: ["SpaceGrotesk", "sans-serif"],
+      satoshi: ["satoshi", "sans-serif"],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        vlocator_purple: {
+          DEFAULT: "#6735FC",
+          1: "",
+        },
+        vlocator_black: {
+          DEFAULT: "#0F1E24",
+          1: "#29333D",
+        },
+        vlocator_gray: {
+          DEFAULT: "#f7f4f0",
+          1: "#A8A7A5",
+          2: "#7E7B85",
+          3: "#DFE1E5",
+          4: "#DAD5CE",
+        },
+        vlocator_green: {
+          DEFAULT: "#60C24F",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
